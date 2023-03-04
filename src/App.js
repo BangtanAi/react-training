4ea0632867b3fde535a2bc186ed135d5b96c7530
KairovaAidana <aidana@kazinsys.kz>
2023-03-04 16:05:40 +0600

    Плавная модалка с анимациями

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import "./index.scss";
 
 function App() {
-  const[open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
   return (
     <div className="App">
-      <button className="open-modal-btn" onClick={()=>setOpen(true)}>✨ Открыть окно</button>
-      {
-        open && <div className="overlay">
+      <button className="open-modal-btn" onClick={() => setOpen(true)}>
+        ✨ Открыть окно
+      </button>
+      <div className={`overlay animated ${open ? 'show' : ''}`}>
         <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200" onClick={()=>setOpen(false)}>
+          <svg
+            height="200"
+            viewBox="0 0 200 200"
+            width="200"
+            onClick={() => setOpen(false)}
+          >
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
         </div>
       </div>
-      }
     </div>
   );
 }
