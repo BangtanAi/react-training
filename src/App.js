d423fb8e1afc3f953947106e527a69a9fcfc6bd5
KairovaAidana <aidana@kazinsys.kz>
2023-03-05 18:05:03 +0600

    Конвертер валют

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import axios from "axios";
+import React from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState("RUB");
+  const [toCurrency, setToCurrency] = React.useState("USD");
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(1);
+  
+  const ratesRef = React.useRef({});
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setToPrice(result.toFixed(2));
+    setFromPrice(value);
+  };
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(2));
+    setToPrice(value);
+  };
+  React.useEffect(() => {
+    axios.get("/currency-db.json").then((res) => {
+      console.log(res.data.rates);
+      ratesRef.current = res.data.rates;
+      onChangeToPrice(1);
+    });
+  }, []);
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+  React.useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeValue={onChangeFromPrice}
+        onChangeCurrency={setFromCurrency}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeValue={onChangeToPrice}
+        onChangeCurrency={setToCurrency}
+      />
     </div>
   );
 }
